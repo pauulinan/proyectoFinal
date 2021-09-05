5f6bbc4ed656ca3675726db878165bafd902bcba
Pau <pauu.linan@gmail.com>
2021-09-05 20:23:15 -0300

    comit inicial del proyecto final

---
+++ funciones.js
@@ -0,0 +1,9 @@
+//la funcion va a pedir ingresar el nombre del producto y va a calcular el iva 
+let zara = 1556;
+let anastasia = 2500;
+let monaco = 2800;
+
+while(true){
+    let precioConjunto = prompt("Ingrese el nombre del conjunto deseado: zara, anastasia o monaco");
+    if
+}
